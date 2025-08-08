a0d1aa853a911a3954d69ea8fe060a79d4f4c96c
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-08 22:48:27 +0545

    index.html

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+    <div className='p-5 mx-4 sm:mx-6 md:mx-12 lg:mx-24 xl:mx-48'>Profile</div>
+    </>
   )
 }
 
